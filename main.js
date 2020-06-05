f11e4403cae2a3d76d70cb499e65df34bda1cadb
Sanvinganza <anton.shvetsov@mail.ru>
2020-06-06 01:33:20 +0300

    add search in google.com

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
-function getInput(){
-    
+let input = document.querySelector('.search');
+const google = 'www.google.com/search?q=';
+
+function runSearch(){
+    document.location.replace(google + input.value);
 }
+
